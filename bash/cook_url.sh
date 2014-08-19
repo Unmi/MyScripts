f3d5ef934ef158c40b436c2a53e672e5f68c9b8f
Yanbin Qiu <fantasia@sina.com>
2014-08-19 13:05:12 -0500

    Update cook_url.sh

--- bash/cook_url.sh
+++ bash/cook_url.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-curl -i $1>$2
-url=$1
-sed -i "/^\s*$/iX-Mob-Source: ${url}" $2
-dos2unix $2
+if [ $# -lt 2 ]; then
+   echo "usage: cook_url url filename"
+else
+   curl -i $1>$2
+   url=$1
+   sed -i "/^\s*$/iX-Mob-Source: ${url}" $2
+   dos2unix -q $2
+fi
