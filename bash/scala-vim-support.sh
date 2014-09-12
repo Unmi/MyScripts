e728842f35c302a9225a9bcf12a79c046c113988
Yanbin Qiu <fantasia@sina.com>
2014-09-11 20:58:22 -0500

    Update scala-vim-support.sh

--- bash/scala-vim-support.sh
+++ bash/scala-vim-support.sh
@@ -2,5 +2,5 @@
 echo "123"
 mkdir -p ~/.vim/{ftdetect,indent,syntax}
 for d in ftdetect indent syntax ;
-  do curl -o ~/.vim/$d/scala.vim https://raw.githubusercontent.com/scala/scala-dist/master/tool-support/src/vim/$d/scala.vim;
+  do curl -s -o ~/.vim/$d/scala.vim https://raw.githubusercontent.com/scala/scala-dist/master/tool-support/src/vim/$d/scala.vim;
 done
